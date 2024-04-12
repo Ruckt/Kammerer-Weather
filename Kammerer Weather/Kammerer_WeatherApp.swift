b328c1d15de38eda741855a2cc6a99e4ba4186d0
Edan Lichtenstein <edan_lichtenstein@comcast.com>
2024-04-12 19:31:22 -0400

    Initial Commit

---
+++ Kammerer Weather/Kammerer_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kammerer_WeatherApp.swift
+//  Kammerer Weather
+//
+//  Created by Lichtenstein, Edan on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Kammerer_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
